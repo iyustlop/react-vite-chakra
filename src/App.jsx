b708d693873fd04ab6f77482ee0b415c901ed7a3
iyustlop <Log1t3ch>
2022-04-05 23:35:49 +0200

    style half applies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import * as API from './services/launch'
-import { Heading } from '@chakra-ui/react';
+import { Heading, Box, Flex, Text, Spacer, Tag } from '@chakra-ui/react';
 
 export function App() {
   const [launches, setLaunches] = useState([]);
@@ -11,14 +11,30 @@ export function App() {
 
   return (
     <>
-      <Heading as="h1" size="lg">SpaceX Missions</Heading>
-      <ul>
+      <Heading as="h1" size="lg" m={4}>
+        SpaceX Missions
+      </Heading>
+      <section>
         {launches.map((launch) => (
-          <li key={launch.flight_number}>
-            {launch.mission_name} ({launch.launch_year})
-          </li>
+          <Box
+            key={launch.flight_number}
+            bg="gray.100"
+            p={4}
+            m={4}
+            borderRadius="lg"
+          >
+            <Flex display="flex">
+              <Text fontSize="2xl">
+                Mission <strong>{launch.mission_name}</strong> ({launch.launch_year})
+              </Text>
+              <Spacer />
+              <Tag p={4} colorScheme="green">
+                {launch.launch_success ? "Successs" : "Failure"}
+              </Tag>
+            </Flex>
+          </Box>
         ))}
-      </ul>
+      </section>
     </>
   );
 }
