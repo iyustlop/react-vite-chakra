74fe716b47118273c33be091727041216c5ace92
iyustlop <Log1t3ch>
2022-04-08 19:17:28 +0200

    Refactor adding Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useState, useEffect } from 'react'
-import * as API from './services/launch'
-import { Heading } from '@chakra-ui/react';
-import { LaunchItem } from './componets/LaunchItem';
+import { Routes, Route } from "react-router-dom";
+import { LaunchList } from './componets/LaunchList'
 
 export function App() {
-  const [launches, setLaunches] = useState([]);
-
-  useEffect(() => {
-    API.getAllLaunches().then(setLaunches);
-  }, []);
-
   return (
     <>
-      <Heading as="h1" size="lg" m={4}>
-        SpaceX Missions
-      </Heading>
-      {
-        launches.length === 0 ? (
-          <div>Loading ...</div>
-        ) : (
-          <section>
-            {launches.map((launch) => (
-              <LaunchItem key={launch.flight_number} {...launch}></LaunchItem>
-            ))}
-          </section>)
-      }
+      <Routes>
+        <Route path="/" element={<LaunchList />} />
+      </Routes>
     </>
   );
 }
---
+++ src/componets/LaunchList.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+import * as API from '../services/launch'
+import { Heading } from '@chakra-ui/react';
+import { LaunchItem } from './LaunchItem';
+
+export function LaunchList() {
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
+        API.getAllLaunches().then(setLaunches);
+    }, []);
+
+    return (
+        <>
+            <Heading as="h1" size="lg" m={4}>
+                SpaceX Missions
+            </Heading>
+            {
+                launches.length === 0 ? (
+                    <div>Loading ...</div>
+                ) : (
+                    <section>
+                        {launches.map((launch) => (
+                            <LaunchItem key={launch.flight_number} {...launch}></LaunchItem>
+                        ))}
+                    </section>)
+            }
+        </>
+    )
+
+}
